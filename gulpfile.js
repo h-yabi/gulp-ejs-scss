a63d24736b15817d2083592b8f289b670f6ede74
h_yabiku <>
2022-12-25 19:59:08 +0900

    add: gulp-webp, gulp-rename

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@
 const { src, dest, watch } = require("gulp");
 // Sassをコンパイルするプラグインを読み込みます
 const sass = require("gulp-sass")(require("sass"));
+const webp = require('gulp-webp');
+const rename = require('gulp-rename');
+
 
 /**
  * Sassをコンパイルするタスクです
@@ -24,5 +27,31 @@ const compileSass = () =>
  */
 const watchSassFiles = () => watch("src/scss/style.scss", compileSass);
 
+/**
+ * imagesをwebpに変換するタスクです
+ */
+const convertWebp = () =>
+  // imagesフォルダー以下の拡張子がjpgかpngのファイルを取得
+  src("src/images/**/*.{jpg,png}")
+    .pipe(rename(function(path) {
+      path.basename += path.extname;
+    }))
+    // webpに変換
+    .pipe(webp())
+    // .pipe(webp({
+    //   // オプションを追加
+    //   quality: 70,
+    //   method: 6,
+    // }))
+    // imagesフォルダー以下に保存
+    .pipe(dest("dist/images"));
+
+/**
+ * imagesファイルを監視し、変更があったらwebpに変換します
+ */
+const watchImagesFiles = () => watch("src/images/**/*.{jpg,png}", convertWebp);
+
+
 // npx gulpというコマンドを実行した時、watchSassFilesが実行されるようにします
 exports.default = watchSassFiles;
+exports.default = watchImagesFiles;
